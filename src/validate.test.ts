654d0b3715caa3dd56ac0b4d429848b805a4c6f9
Erik Brinkman <erik.brinkman@gmail.com>
2024-04-04 00:08:21 -0400

    add validate test

---
+++ src/validate.test.ts
@@ -0,0 +1,18 @@
+import { UploadEntry } from ".";
+import { JtdSchema, validate } from "./validate";
+
+const uploadEntrySchema: JtdSchema<UploadEntry> = {
+  properties: {
+    docID: { type: "string" },
+    hash: { type: "string" },
+  },
+};
+
+test("validate()", () => {
+  const accurate = { docID: "", hash: "" };
+  expect(() => validate(uploadEntrySchema, accurate)).not.toThrow();
+
+  const innaccurate = { docID: "" };
+  expect(() => validate(uploadEntrySchema, innaccurate)).toThrow();
+  expect(() => validate(uploadEntrySchema, innaccurate, false)).not.toThrow();
+});
